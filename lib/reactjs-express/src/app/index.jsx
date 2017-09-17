f8f10bc598961eaada4e863b14c6b98d2405055d
Kevin Wynn <lukevinskywynn@gmail.com>
2017-09-17 16:04:27 -0500

    updating users test data

--- lib/reactjs-express/src/app/index.jsx
+++ lib/reactjs-express/src/app/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import User from './components/User.jsx';
+
 class App extends React.Component {
   render () {
     return (
       <div className="home">
-        <h1>Hello!</h1>
+        <User />
         <p>Welcome to the ReactJS and ExpressJS generator</p>
         <p>Check out the <a href="/docs">documentation</a> to get started.</p>
       </div>
