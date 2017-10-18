7a994faa7fd2a9de84312cd33800945517856a4e
Kevin Wynn <kevin@outboundengine.com>
2017-10-18 11:31:36 -0500

    fix install instructions

--- lib/reactjs-express-multipage/src/app/pages/Home.jsx
+++ lib/reactjs-express-multipage/src/app/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import User from './components/User.jsx';
+import User from '../components/User.jsx';
 
 class Home extends React.Component {
   render () {
